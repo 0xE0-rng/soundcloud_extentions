28fa381082fa6493a1ec452ca290c2df6be7cf37
Luis <xerg0n@users.noreply.github.com>
2021-03-25 01:14:57 +0000

    Update soundcloud_extentions.user.js
    added sort by reposts, more stable insert logic

--- soundcloud_extentions.user.js
+++ soundcloud_extentions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SoundCloud Extentions
 // @namespace    http://tampermonkey.net/
-// @version      0.15
+// @version      0.20
 // @description  Only works on the soundcloud search page so far, sorts loades songs by likes
 // @author       xerg0n
 // @match        https://soundcloud.com/*
@@ -10,10 +10,14 @@
 (function() {
     'use strict';
     var append = function(){
-        var node = document.createElement("button");
-        node.className = "sc-button sc-button-small sc-button-responsive"
-        node.innerHTML = "Sort By Likes"
-        node.onclick = function (){
+        var container = document.createElement("div")
+        container.id = "filterbox"
+        container.style = "float: right;"
+
+        var select = document.createElement("select")
+        var option1 = document.createElement("option")
+        option1.innerHTML = "▼ order by likes"
+        option1.onclick = function (){
             var list = Array.from(document.querySelectorAll('li div.searchItem__trackItem'))
             var sorted = [...list].sort(function(a, b) {
                 var a_likes = parseNumber(a.querySelector('.sc-button-like').innerText)
@@ -25,7 +29,28 @@
                 list[i].parentNode.appendChild(sorted[i])
             }
         }
-        document.querySelector('.searchResultGroupHeading').appendChild(node)
+        var option2 = document.createElement("option")
+        option2.innerHTML = "▼ order by reposts"
+        option2.onclick = function (){
+            var list = Array.from(document.querySelectorAll('li div.searchItem__trackItem'))
+            var sorted = [...list].sort(function(a, b) {
+                var a_likes = parseNumber(a.querySelector('.sc-button-repost').innerText)
+                var b_likes = parseNumber(b.querySelector('.sc-button-repost').innerText)
+                return b_likes-a_likes;
+            });
+
+            for (var i = 0; i < list.length; i++) {
+                list[i].parentNode.appendChild(sorted[i])
+            }
+        }
+        select.appendChild(option1)
+        select.appendChild(option2)
+        select.style = "appearance: none;"
+        select.className = "sc-button sc-button-small"
+
+        container.appendChild(select)
+        document.querySelector('.searchTitle__text').appendChild(container)
+        console.log("appended")
     }
     function parseNumber(number){
         if (number.match(/,.*K$/g)){
@@ -38,8 +63,20 @@
             await new Promise(r => setTimeout(r, 500));
         }
     }
-    function appyWaitFor(){
-        waitFor(".searchResultGroupHeading").then(append);
+    function changeListener(){
+        console.log("oh?")
+            waitFor(".searchTitle__text").then(maybeappend);
     }
-    window.addEventListener("change", appyWaitFor);
+    function maybeappend(){
+         var elementExists = document.getElementById("filterbox");
+        if (!elementExists){
+            append()
+        }
+    }
+    var elements = document.querySelectorAll("a");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", changeListener)
+    }
+    window.addEventListener("change", changeListener);
+    window.addEventListener("load", changeListener);
 })();
